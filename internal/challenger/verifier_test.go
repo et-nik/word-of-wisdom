2d289710864f7eae43f336264afebcc7c35dc95c
et-nik <nikita.hldm@gmail.com>
2024-01-23 01:08:57 +0100

    fix test

--- internal/challenger/verifier_test.go
+++ internal/challenger/verifier_test.go
@@ -1,10 +1,12 @@
 package challenger_test
 
 import (
+	"encoding/binary"
 	"testing"
 
 	"github.com/et-nik/word-of-wisdom/internal/challenger"
 	"github.com/et-nik/word-of-wisdom/internal/domain"
+	"github.com/et-nik/word-of-wisdom/pkg/equihash"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,13 +15,13 @@ func TestNewVerifier_Positive(t *testing.T) {
 
 	result := v.Verify(
 		domain.Challenge{
-			N:    54,
-			K:    2,
-			Seed: []byte{4, 1, 2, 3, 41, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+			N:    52,
+			K:    3,
+			Seed: equihash.Uint32ArrayToBytes([]uint32{1, 1, 1, 1}, binary.LittleEndian),
 		},
 		domain.Solution{
 			Nonce: 2,
-			Hash:  []byte{0x31, 0x3a, 0x1, 0x0, 0x67, 0xa9, 0x2, 0x0, 0xe7, 0x42, 0x4, 0x0, 0x5e, 0xd9, 0x6, 0x0},
+			Hash:  equihash.Uint32ArrayToBytes([]uint32{0x611, 0x1626, 0x1c37, 0x20cb, 0x241d, 0x30d7, 0x3811, 0x395c}, binary.LittleEndian),
 		},
 	)
 
